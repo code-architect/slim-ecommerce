fcf0c4cdf444aeb5b3d382af10257da97c6e8931
code-architect <codearchitectdeveloper@gmail.com>
2016-12-25 20:07:30 +0530

    added the TwigExtension
    TwigExtension are responsible for within our views , to create a link to a page, we can use our router within their to build a new path or we can any of the other helper s are available.

--- app/container.php
+++ app/container.php
@@ -2,6 +2,7 @@
 
 use Slim\Views\Twig;
 use \Interop\Container\ContainerInterface;
+use Slim\Views\TwigExtension;
 
 return [
     'router'    => get(Slim\Router::class),
@@ -9,5 +10,13 @@ return [
         $twig = new Twig(__DIR__ . '/../resources/views', [
             'cache' =>  false   //for development
         ]);
+
+
+        $twig->addExtension(new TwigExtension(
+            $c->get('router'),
+            $c->get('request')->getUri()
+        ));
+
+        return $twig;
     }
 ];
