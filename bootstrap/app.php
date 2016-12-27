6a6c0dfbff61ebdc8e3974ca2456029543748e08
code-architect <codearchitectdeveloper@gmail.com>
2016-12-27 13:57:57 +0530

    pulling eloquent in. setting up database connection.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,11 +1,29 @@
 <?php
 
 use Cart\App;
-
+use Illuminate\Database\Capsule\Manager as Capsule;
 session_start();
 
 require __DIR__.'/../vendor/autoload.php';
 
 $app = new App;
+
+//pulling eloquent in
+$capsule = new Capsule;
+
+//connection to DB
+$capsule->addConnection([
+    'driver'    =>  'mysql',
+    'host'      =>  'localhost',
+    'database'  =>  'slim_ecommerce',
+    'username'  =>  'root',
+    'password'  =>  '',
+    'charset'   =>  'utf8',
+    'collation' =>  'utf8_unicode_ci',
+    'prefix'    =>  ''
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 require __DIR__ .'/../App/routes.php';
