a81c6c930f4169a7319f987d0a96912a2fca4d51
code-architect <codearchitectdeveloper@gmail.com>
2017-01-05 17:40:37 +0530

    injecting twig and routes globally using a constructor.
    Showing data in product.twig view

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace Cart\Controllers;
 
+use Slim\Router;
 use Cart\Models\Product;
 use Slim\Views\Twig;
 use Psr\Http\Message\ResponseInterface as Response;
@@ -9,10 +10,30 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ProductController
 {
-
-    public function get( $slug, Request $request, Response $response, Twig $view, Product $product )
+    protected $view;
+    protected $router;
+    public function __construct(Twig $view, Router $router)
     {
-        return $view->render($response, 'products\product.twig');
+        // getting the view and router globally, so we don't have to inject it in every method
+        $this->view = $view;
+        $this->router = $router;
+    }
+
+
+    public function get( $slug, Request $request, Response $response, Product $product )
+    {
+        // fetching data by product slug
+        $product = $product->where('slug', $slug)->first();
+
+        // if no product found, redirect
+        if(!$product)
+        {
+            return $response->withRedirect($this->router->pathFor('home'));
+        }
+
+        return $this->view->render($response, 'products\product.twig', [
+            'product'   =>  $product,
+        ]);
     }
 
 }
